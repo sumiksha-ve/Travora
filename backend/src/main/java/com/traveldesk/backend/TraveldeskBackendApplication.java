6c237c59c210e851e8dc41f4786e004a610fa47b
Haripreeth27 <haripreeth2720@gmail.com>
2026-08-24 23:47:18 +0530

    Set up Spring Boot backend

---
+++ backend/src/main/java/com/traveldesk/backend/TraveldeskBackendApplication.java
@@ -0,0 +1,13 @@
+package com.traveldesk.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraveldeskBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraveldeskBackendApplication.class, args);
+	}
+
+}
